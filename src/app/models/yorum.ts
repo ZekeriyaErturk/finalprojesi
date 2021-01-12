e36935652455b120b28bb98ccc59f20755d58ed2
zekeriya <zekeriyaerturk@gmail.com>
2021-01-12 20:14:37 +0300

    Yorum servisi eklendi

---
+++ src/app/models/yorum.ts
@@ -0,0 +1,9 @@
+export class Yorum {
+  key: string;
+  uid: string;
+  postid: string;
+  yorum: string;
+  likes: number = 0;
+  kayTarih: string;
+  duzTarih: string;
+}
--- src/app/services/yorum.service.ts
+++ src/app/services/yorum.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Yorum } from '../models/yorum';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class YorumService {
+export class CommentsService {
+  private dbComment = '/Yorumlar';
+  yorumRef: AngularFireList<Yorum>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+    this.yorumRef = db.list(this.dbComment);
+  }
+
+  YorumListele() {
+    return this.yorumRef;
+  }
+
+  YorumListeleByPostID(postId: string) {
+    return this.db.list('Yorumlar', (q) =>
+      q.orderByChild('postid').equalTo(postId)
+    );
+  }
+
+  YorumListeleByUID(uid: string) {
+    return this.db.list('Yorumlar', (q) => q.orderByChild('uid').equalTo(uid));
+  }
+
+  YorumEkle(yorum: Yorum) {
+    return this.yorumRef.push(yorum);
+  }
+
+  YorumDüzenle(yorum: Yorum) {
+    return this.yorumRef.update(yorum.key, yorum);
+  }
+
+  YorumSil(key: string) {
+    return this.yorumRef.remove(key);
+  }
 }
