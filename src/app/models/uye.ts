0283e8d339c6312a09b0c9e19fffcd6a62e0cb08
zekeriya <zekeriyaerturk@gmail.com>
2021-01-12 20:13:24 +0300

    Uyelik Servis eklendi

---
+++ src/app/models/uye.ts
@@ -0,0 +1,7 @@
+export class Uye {
+  key: string;
+  adsoyad: string;
+  mail: string;
+  uid: string;
+  rol: string = 'üye';
+}
--- src/app/services/uyelik.service.ts
+++ src/app/services/uyelik.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Uye } from '../models/uye';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UyelikService {
+  user: Observable<any>;
+  private dbUye = '/Uyeler';
+  uyeRef: AngularFireList<Uye>;
 
-  constructor() { }
+  constructor(
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
+  ) {
+    this.user = this.afAuth.authState;
+    this.uyeRef = db.list(this.dbUye);
+  }
+
+  OturumAc(mail: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(mail, password);
+  }
+
+  UyeOl(mail: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(mail, password);
+  }
+
+  UyeEkle(uye: Uye) {
+    return this.uyeRef.push(uye);
+  }
+
+  OturumKapat() {
+    return this.afAuth.signOut();
+  }
 }
