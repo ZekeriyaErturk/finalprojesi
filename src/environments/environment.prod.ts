26b3c1b48eb10e6c14ba7aa42fd7bf81752ffdc6
zekeriya <zekeriyaerturk@gmail.com>
2021-01-14 16:59:44 +0300

    deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyALgevCWxbWt7xLLvBfNjluk5g8C3tWJHw',
+    authDomain: 'internet-programciligi-final.firebaseapp.com',
+    databaseURL:
+      'https://internet-programciligi-final-default-rtdb.firebaseio.com',
+    projectId: 'internet-programciligi-final',
+    storageBucket: 'internet-programciligi-final.appspot.com',
+    messagingSenderId: '268929231901',
+    appId: '1:268929231901:web:5e62f1aa3bd868933baaf4',
+  },
 };
